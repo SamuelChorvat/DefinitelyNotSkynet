e4a381a3eee41fdf3193cfaf08bf77c31ced8995
Samuel Chorvat <samuel.chorvat@defra.onmicrosoft.com>
2023-05-22 10:16:59 +0100

    Update create note

--- SkyNotes.Common.EntityModels.Sqlite/Note.cs
+++ SkyNotes.Common.EntityModels.Sqlite/Note.cs
@@ -16,10 +16,16 @@ namespace SkyNotes.Common.EntityModels.Sqlite
 
         [Required] public DateTime CreatedAt { get; set; }
 
-        [Required] [StringLength(32)] public string? RelatesTo { get; set; }
+        
+        [Display(Name = "Relates To")]
+        [Required(ErrorMessage = "The Relates To field is required.")]
+        [StringLength(32)] 
+        public string? RelatesTo { get; set; }
 
         [StringLength(32)] public string? TicketId { get; set; }
 
-        [Required] public string? NoteText { get; set; }
+        [Required(ErrorMessage = "The Content field is required.")]
+        [Display(Name = "Content")] 
+        public string? NoteText { get; set; }
     }
 }
