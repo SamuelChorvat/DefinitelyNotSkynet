0ce17403368729a0d74e4433d488ccd722e9c55c
Samuel Chorvat <samuel.chorvat@defra.onmicrosoft.com>
2023-05-22 13:59:43 +0100

    Delete note

--- SkyNotes.BlazorServer/Data/ISkyNotesService.cs
+++ SkyNotes.BlazorServer/Data/ISkyNotesService.cs
@@ -5,5 +5,7 @@ namespace SkyNotes.BlazorServer.Data;
 public interface ISkyNotesService
 {
     Task<List<Note>> GetNotesAsync();
+    Task<Note?> GetNoteAsync(int id);
     Task<Note> CreateNoteAsync(Note note);
+    Task DeleteNoteAsync(int id);
 }
--- SkyNotes.BlazorServer/Data/SkyNotesService.cs
+++ SkyNotes.BlazorServer/Data/SkyNotesService.cs
@@ -18,11 +18,31 @@ public class SkyNotesService : ISkyNotesService
         return _dbContext.Notes.OrderByDescending(x => x.CreatedAt).ToListAsync();
     }
 
+    public Task<Note?> GetNoteAsync(int id)
+    {
+        return _dbContext.Notes.FirstOrDefaultAsync(n => n.NoteId == id);
+    }
+
     public Task<Note> CreateNoteAsync(Note note)
     {
         if (string.IsNullOrEmpty(note.TicketId)) note.TicketId = "N/A";
         _dbContext.Notes.Add(note);
         _dbContext.SaveChangesAsync();
         return Task.FromResult(note);
+    }
+
+    public Task DeleteNoteAsync(int id)
+    {
+        Note? note = _dbContext.Notes.FirstOrDefaultAsync(n => n.NoteId == id).Result;
+
+        if (note == null)
+        {
+            return Task.CompletedTask;
+        }
+        else
+        {
+            _dbContext.Notes.Remove(note);
+            return _dbContext.SaveChangesAsync();
+        }
     }
 }
